079d1cee34110f8e485dbdbd11bbdc34e8d660de
Mike Giannopoulos <sgiannop@gmail.com>
2023-02-02 21:30:13 +0200

    added xdebug functionality and applied migrations.

--- migrations/Version20230202161504.php
+++ migrations/Version20230202161504.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230201190450 extends AbstractMigration
+final class Version20230202161504 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -26,8 +26,10 @@ final class Version20230201190450 extends AbstractMigration
         $this->addSql('CREATE TABLE "AppUsers" (id INT NOT NULL, first_name VARCHAR(255) DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE "Movies" (id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN "Movies".created_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE TABLE "Votes" (id INT NOT NULL, app_user_id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "Votes" (id INT NOT NULL, app_user_id INT NOT NULL, movie_id INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_904A55CB4A3353D8 ON "Votes" (app_user_id)');
+        $this->addSql('CREATE INDEX IDX_904A55CB8F93B6FC ON "Votes" (movie_id)');
+        $this->addSql('COMMENT ON COLUMN "Votes".created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
         $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
@@ -41,6 +43,7 @@ final class Version20230201190450 extends AbstractMigration
         $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
         $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
         $this->addSql('ALTER TABLE "Votes" ADD CONSTRAINT FK_904A55CB4A3353D8 FOREIGN KEY (app_user_id) REFERENCES "AppUsers" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE "Votes" ADD CONSTRAINT FK_904A55CB8F93B6FC FOREIGN KEY (movie_id) REFERENCES "Movies" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
@@ -51,6 +54,7 @@ final class Version20230201190450 extends AbstractMigration
         $this->addSql('DROP SEQUENCE "Movies_id_seq" CASCADE');
         $this->addSql('DROP SEQUENCE "Votes_id_seq" CASCADE');
         $this->addSql('ALTER TABLE "Votes" DROP CONSTRAINT FK_904A55CB4A3353D8');
+        $this->addSql('ALTER TABLE "Votes" DROP CONSTRAINT FK_904A55CB8F93B6FC');
         $this->addSql('DROP TABLE "AppUsers"');
         $this->addSql('DROP TABLE "Movies"');
         $this->addSql('DROP TABLE "Votes"');
